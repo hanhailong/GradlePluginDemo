0b18ee32a386d5caeb5e71cdd49d240ba513f2ab
hanhailong <hanhailong.cool@163.com>
2017-08-06 14:40:59 +0800

    优化项目

--- sample/src/main/groovy/com/hhl/sample/Main.groovy
+++ sample/src/main/groovy/com/hhl/sample/Main.groovy
@@ -2,9 +2,18 @@ package com.hhl.sample
 
 import com.hhl.sample.ext.DemoExtension
 
+
 class Main {
-
-    DemoExtension demoExtension = new DemoExtension()
-
 //    demoExtension.setName ( "xxxx" );
+
+    public static void main(String[] args) {
+        DemoExtension demoExtension = new DemoExtension()
+
+        demoExtension.name = "abc"
+
+        demoExtension.version = 1000
+
+        println(demoExtension.name)
+        println(demoExtension.version)
+    }
 }
